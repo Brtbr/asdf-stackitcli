1937a80fa0e3c26f0bdc4a4bc0cc92ff5ce595c2
Brtbr <login-github@jsmg.de>
2025-03-25 23:47:17 +0100

    fix: restructre code

--- lib/utils.bash
+++ lib/utils.bash
@@ -1,16 +1,8 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
-
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for stackitcli.
 GH_REPO="https://github.com/stackitcloud/stackit-cli"
 TOOL_NAME="stackitcli"
 TOOL_TEST="stackit --version"
-
-fail() {
-	echo -e "asdf-$TOOL_NAME: $*"
-	exit 1
-}
 
 curl_opts=(-fsSL)
 
@@ -19,33 +11,28 @@ if [ -n "${GITHUB_API_TOKEN:-}" ]; then
 	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
 fi
 
-sort_versions() {
-	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
-		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
+fail() {
+	echo -e "asdf-$TOOL_NAME: $*"
+	exit 1
 }
 
-list_github_tags() {
-	git ls-remote --tags --refs "$GH_REPO" |
-		grep -o 'refs/tags/.*' | cut -d/ -f3- |
-		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+get_cpu() {
+  local machine_hardware_name
+  machine_hardware_name=${ASDF_STACKITCLI_OVERWRITE_ARCH:-"$(uname -m)"}
+
+  case "$machine_hardware_name" in
+  'x86_64') local cpu_type="amd64" ;;
+  'powerpc64le' | 'ppc64le') local cpu_type="ppc64le" ;;
+  'aarch64') local cpu_type="arm64" ;;
+  'armv7l') local cpu_type="arm" ;;
+  *) local cpu_type="$machine_hardware_name" ;;
+  esac
+
+  echo "$cpu_type"
 }
 
-list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if stackitcli has other means of determining installable versions.
-	list_github_tags
-}
-
-download_release() {
-	local version filename url
-	version="$1"
-	filename="$2"
-
-	# TODO: Adapt the release URL convention for stackitcli
-	url="$GH_REPO/releases/download/v${version}/stackit-cli_${version}_$(get_arch)_$(get_cpu).tar.gz"
-
-	echo "* Downloading $TOOL_NAME release $version..."
-	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
+get_arch() {
+  uname | tr '[:upper:]' '[:lower:]'
 }
 
 install_version() {
@@ -62,7 +49,7 @@ install_version() {
 		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
 		chmod +x "$install_path/stackit"
-		# TODO: Assert stackitcli executable exists.
+
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
 		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
@@ -74,21 +61,17 @@ install_version() {
 	)
 }
 
-get_cpu() {
-  local machine_hardware_name
-  machine_hardware_name=${ASDF_TELEPRESENCE_OVERWRITE_ARCH:-"$(uname -m)"}
-
-  case "$machine_hardware_name" in
-    'x86_64') local cpu_type="amd64" ;;
-    'powerpc64le' | 'ppc64le') local cpu_type="ppc64le" ;;
-    'aarch64') local cpu_type="arm64" ;;
-    'armv7l') local cpu_type="arm" ;;
-    *) local cpu_type="$machine_hardware_name" ;;
-  esac
-
-  echo "$cpu_type"
+sort_versions() {
+	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
+		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
 }
 
-get_arch() {
-  uname | tr '[:upper:]' '[:lower:]'
+list_github_tags() {
+	git ls-remote --tags --refs "$GH_REPO" |
+		grep -o 'refs/tags/.*' | cut -d/ -f3- |
+		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+}
+
+list_all_versions() {
+	list_github_tags
 }
